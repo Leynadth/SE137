66548c942cc4edc6b0bd24f4cdfa2d0d46ed01e0
Leynadth <139769891+Leynadth@users.noreply.github.com>
2023-10-16 09:06:03 -0400

    w2

--- c#/lab1.cs
+++ c#/lab1.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using system. Threading. Tasks;
 
 namespace StudentGradeCalculator
 {
@@ -6,16 +10,13 @@ namespace StudentGradeCalculator
     {
         static void Main(string[] args)
         {
-            // Program information
             Console.WriteLine("Student Grade Calculator");
             Console.WriteLine("Date: " + DateTime.Now.ToString("yyyy-MM-dd"));
             Console.WriteLine("Description: Calculate average grade and assign a letter grade.");
 
-            // Gather student information
             Console.Write("Enter student name: ");
             string studentName = Console.ReadLine();
 
-            // Gather grades and convert to numeric form
             Console.Write("Enter grade for Assignment 1 (out of 100): ");
             string grade1Input = Console.ReadLine();
             double grade1 = Convert.ToDouble(grade1Input);
@@ -32,10 +33,8 @@ namespace StudentGradeCalculator
             string finalInput = Console.ReadLine();
             double finalExam = Convert.ToDouble(finalInput);
 
-            // Calculate average based on weightings
             double average = (grade1 * 0.20 + grade2 * 0.20 + midterm * 0.25 + finalExam * 0.35);
 
-            // Assign letter grade based on the average
             string letterGrade;
             if (average >= 90)
             {
@@ -58,12 +57,10 @@ namespace StudentGradeCalculator
                 letterGrade = "F";
             }
 
-            // Output results
             Console.WriteLine("\nStudent Name: " + studentName);
             Console.WriteLine("Average Grade: " + average.ToString("0.00"));
             Console.WriteLine("Letter Grade: " + letterGrade);
 
-            // Allow the user to see the results before exiting
             Console.WriteLine("\nPress any key to exit...");
             Console.ReadKey();
         }
